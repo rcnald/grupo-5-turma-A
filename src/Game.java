7d1b33d1bc6f67d15c8c4cef7c8a66c241ca4d1f
Ronaldo <ronaldomjunior05@gmail.com>
2024-11-01 14:52:39 -0300

    feat: finish introduction dialog

--- src/Game.java
+++ src/Game.java
@@ -1,18 +1,13 @@
-import java.lang.reflect.Method;
 import java.util.Scanner;
-import java.util.function.Function;
 
 interface Interacao {
   void executar();
 }
 
-// TODO: fazer metodos por exemplo. introducao, dialogo, inicial, respostaA,
-// respostaB, salvar os valores das variaveis atuais, acho que retornar seria
-// melhor, para assim manipular as perguntas da forma que eu quiser, puder
-// repetilas sem alteras os atributos entende.
+// TODO: mostrar nivel de confianca de taka, e "mostrar" ao jogador como isso impacta na historia. e claro toda escolha deve ter uma vantagem e uma desvantagem
 
 public class Game {
-  private String resposta;
+  private String resposta = "";
   private int nivelDeConfianca = 5;
 
   Scanner entrada = new Scanner(System.in);
@@ -30,7 +25,7 @@ public class Game {
 
       System.out.print("Escolha uma opcao: ");
 
-      this.obterRespostaDoJogadorNoMenu();
+      obterRespostaDoJogadorNoMenu();
 
       switch (resposta) {
         case "1":
@@ -83,33 +78,28 @@ public class Game {
 
   }
 
-  public void obterRespostaDoJogador(Interacao interacao) {
+  public void obterRespostaDoJogador() {
     System.out.println("\n");
     System.out.println("Digite sua resposta: (Digite 'menu' para voltar ao menu.)");
 
-    if(!resposta.equalsIgnoreCase("menu")){
+    if (!resposta.equalsIgnoreCase("menu")) {
       resposta = entrada.next();
     }
-
-    this.verificarSeRespostaForMenu(interacao);
-  }
-
-  public boolean respostaPositiva(String respostas){
-    return respostas.equalsIgnoreCase("s");
-  }
-
-  public boolean respostaNegativa(String respostas){
-    return respostas.equalsIgnoreCase("n");
   }
 
   public void obterRespostaDoJogadorNoMenu() {
-    System.out.print("Escolha uma opcao: ");
-
     resposta = entrada.next();
-    this.limparTerminal();
   }
 
-  public void verificarSeRespostaForMenu(Interacao interacao) {
+  public boolean respostaPositiva(String respostas) {
+    return respostas.equalsIgnoreCase("s");
+  }
+
+  public boolean respostaNegativa(String respostas) {
+    return respostas.equalsIgnoreCase("n");
+  }
+
+  public void confirmarVoltarAoMenu(Interacao interacao) {
     if (resposta.equalsIgnoreCase("menu")) {
       boolean respostaExiste = false;
 
@@ -117,16 +107,15 @@ public class Game {
         System.out.print("Tem certeza que deseja voltar ao menu principal? (s/n): ");
         String confirmacao = entrada.next();
 
-        this.limparTerminal();
-
         if (respostaPositiva(confirmacao)) {
-          
+          this.limparTerminal();
 
           respostaExiste = true;
           resposta = "";
 
           this.menu();
         } else if (respostaNegativa(confirmacao)) {
+          this.limparTerminal();
 
           respostaExiste = true;
           resposta = "";
@@ -135,37 +124,41 @@ public class Game {
         } else {
 
           respostaExiste = false;
-          
+
           System.out.println("Opção inválida. Tente novamente. verificar se for menu");
 
-          interacao.executar();
         }
       } while (!respostaExiste);
 
     }
   }
 
-  // public class Introducao {
-  // public void mostrarIntroducao() {
-  // System.out.println("Bem-vindo, Nys!");
-  // System.out.println(
-  // "Desde a perda dos seus pais, você tem vivido isolado. Seu único companheiro
-  // é Taka, seu robô guardião.");
-  // System.out.println(
-  // "Mas algo não parece certo. Aos poucos, você começa a desconfiar que Taka
-  // guarda segredos sobre o acidente de seus pais.");
+  public boolean respostaIgualMenu() {
+    return resposta.equalsIgnoreCase("menu");
+  }
 
-  // primeiraInteracaoTaka();
-  // }
+  public class Introducao {
+    public void executar() {
+      System.out.println("Bem-vindo, Nys!");
+      System.out.println(
+          "Desde a perda dos seus pais, você tem vivido isolado. Seu único companheiro  é Taka, seu robô guardião.");
+      System.out.println(
+          "Mas algo não parece certo. Aos poucos, você começa a desconfiar que Taka  guarda segredos sobre o acidente de seus pais.");
+
+      PrimeiraInteracaoTaka primeiraInteracao = new PrimeiraInteracaoTaka();
+
+      primeiraInteracao.executar();
+    }
+  }
 
   public class PrimeiraInteracaoTaka implements Interacao {
 
     @Override
     public void executar() {
       boolean respostaExiste = false;
+      SegundaInteracaoTaka segundaInteracao = new SegundaInteracaoTaka();
 
       do {
-        // limparTerminal();
 
         System.out.println("\nTaka entra na sala e pergunta:");
         System.out.println("\"Tudo bem com você, Nys?\"");
@@ -173,81 +166,126 @@ public class Game {
         System.out.println("2 - Responder de forma suspeita");
         System.out.print("Escolha uma opção: ");
 
-        obterRespostaDoJogador(this);
+        obterRespostaDoJogador();
+
+        if (respostaIgualMenu()) {
+          confirmarVoltarAoMenu(this);
+          return;
+        }
 
         if (resposta.equalsIgnoreCase("1")) {
+          limparTerminal();
           respostaExiste = true;
 
           System.out.println("Você responde amigavelmente, mas, por dentro, continua desconfiando.");
           nivelDeConfianca += 2;
+          segundaInteracao.executar();
         } else if (resposta.equalsIgnoreCase("2")) {
+          limparTerminal();
           respostaExiste = true;
 
-          System.out.println("Você responde de forma suspeita. Taka observa, mas não reage.");
+          System.out.println("Você responde de forma suspeita. Taka observa, mas nao reage.");
           nivelDeConfianca -= 2;
+          segundaInteracao.executar();
         } else {
           respostaExiste = false;
 
-          System.out.println("Opção inválida. Tente novamente. primeira interacao");
+          System.out.println("Opcao inválida. Tente novamente.");
         }
       } while (!respostaExiste);
-
-      // limparTerminal();
     }
-
   }
 
-  // private void primeiraInteracaoTaka() {
-  // System.out.println("\nTaka entra na sala e pergunta:");
-  // System.out.println("\"Tudo bem com você, Nys?\"");
-  // System.out.println("1 - Responder amigavelmente");
-  // System.out.println("2 - Responder de forma suspeita");
-  // System.out.print("Escolha uma opção: ");
+  public class SegundaInteracaoTaka implements Interacao {
 
-  // obterRespostaDoJogador(this.primeiraInteracaoTaka);
+    @Override
+    public void executar() {
+      boolean respostaExiste = false;
+      TerceiraInteracaoTaka terceiraInteracao = new TerceiraInteracaoTaka();
 
-  // if (resposta.equalsIgnoreCase("1")) {
-  // System.out.println("Você responde amigavelmente, mas, por dentro, continua
-  // desconfiando.");
-  // nivelDeConfianca += 2;
-  // } else if (resposta.equalsIgnoreCase("2")) {
-  // System.out.println("Você responde de forma suspeita. Taka observa, mas não
-  // reage.");
-  // nivelDeConfianca -= 2;
-  // }
+      do {
+        System.out.println(
+            "\nMais tarde naquela noite, você encontra um arquivo no seu computador com o  nome dos seus pais. O arquivo está encriptado.");
+        System.out.println("1 - Tentar hackear o arquivo agora");
+        System.out.println("2 - Perguntar a Taka sobre o arquivo");
+        System.out.print("Escolha uma opcao: ");
 
-  // segundaInteracaoArquivo();
-  // }
+        obterRespostaDoJogador();
 
-  // private void segundaInteracaoArquivo() {
-  // System.out.println(
-  // "\nMais tarde naquela noite, você encontra um arquivo no seu computador com o
-  // nome dos seus pais. O arquivo está encriptado.");
-  // System.out.println("1 - Tentar hackear o arquivo agora");
-  // System.out.println("2 - Perguntar a Taka sobre o arquivo");
-  // System.out.print("Escolha uma opção: ");
+        if (respostaIgualMenu()) {
+          confirmarVoltarAoMenu(this);
+          return;
+        }
 
-  // obterRespostaDoJogador();
+        if (resposta.equalsIgnoreCase("1")) {
+          limparTerminal();
+          respostaExiste = true;
+          System.out.println(
+              "Você tenta hackear o arquivo, mas percebe que o nível de encriptacao é alto demais para suas habilidades atuais.");
+          System.out.println("Você precisará estudar mais antes de tentar novamente.");
+          terceiraInteracao.executar();
+        } else if (resposta.equalsIgnoreCase("2")) {
+          limparTerminal();
+          respostaExiste = true;
+          System.out.println("Você pergunta a Taka sobre o arquivo.");
 
-  // if (resposta.equalsIgnoreCase("1")) {
-  // System.out.println(
-  // "Você tenta hackear o arquivo, mas percebe que o nível de encriptação é alto
-  // demais para suas habilidades atuais.");
-  // System.out.println("Você precisará estudar mais antes de tentar novamente.");
-  // } else if (resposta.equalsIgnoreCase("2")) {
-  // System.out.println("Você pergunta a Taka sobre o arquivo.");
-  // if (nivelDeConfianca > 5) {
-  // System.out.println("Taka hesita, mas responde: \"É apenas um arquivo antigo.
-  // Não se preocupe com isso.\"");
-  // } else {
-  // System.out.println(
-  // "Taka responde friamente: \"Não tenho informações sobre isso.\" Você sente
-  // que ele está escondendo algo.");
-  // nivelDeConfianca -= 1;
-  // }
-  // }
-  // }
-  // }
+          if (nivelDeConfianca > 5) {
+            System.out.println("Taka hesita, mas responde: \"É apenas um arquivo antigo. Nao se preocupe com isso.\"");
+          } else {
+            System.out.println(
+                "Taka responde friamente: \"Nao tenho informacoes sobre isso.\" Você sente que ele está escondendo algo.");
+            nivelDeConfianca -= 1;
+          }
+
+          terceiraInteracao.executar();
+        } else {
+          respostaExiste = false;
+          System.out.println("Opcao inválida. Tente novamente.");
+        }
+
+      } while (!respostaExiste);
+    }
+  }
+
+  public class TerceiraInteracaoTaka implements Interacao {
+
+    @Override
+    public void executar() {
+      boolean respostaExiste = false;
+
+      do {
+        System.out.println(
+            "\nFrustrado, você decide começar a desenvolver um programa em Java para desativar temporariamente Taka e acessar os arquivos escondidos.");
+        System.out.println("1 - Trabalhar no programa durante toda a noite");
+        System.out.println("2 - Desistir e esperar outro dia");
+        System.out.print("Escolha uma opcao: ");
+
+        obterRespostaDoJogador();
+
+        if (respostaIgualMenu()) {
+          confirmarVoltarAoMenu(this);
+          return;
+        }
+
+        if (resposta.equalsIgnoreCase("1")) {
+          limparTerminal();
+          respostaExiste = true;
+          System.out.println(
+              "Você passa a noite escrevendo linhas de código, focado em criar um programa que possa desativar Taka.");
+          System.out.println("Sua habilidade de programacao aumenta.");
+        } else if (resposta.equalsIgnoreCase("2")) {
+          limparTerminal();
+          respostaExiste = true;
+          System.out.println(
+              "Você decide deixar para outro dia. Uma sensacao de incerteza te atormenta enquanto você tenta dormir.");
+        } else {
+          respostaExiste = false;
+          System.out.println("Opção inválida. Tente novamente.");
+        }
+
+      } while (!respostaExiste);
+    }
+  }
 
   public void instructions() {
     System.out.println(
@@ -263,83 +301,11 @@ public class Game {
   }
 
   public void start() {
-    Interacao introducao = new PrimeiraInteracaoTaka();
+    Introducao introducao = new Introducao();
     introducao.executar();
-    // System.out.println("Bem-vindo, Nys!");
-    // System.out.println(
-    // "Desde a perda dos seus pais, você tem vivido isolado. Seu único companheiro
-    // é Taka, seu robô guardião.");
-    // System.out.println(
-    // "Mas algo não parece certo. Aos poucos, você começa a desconfiar que Taka
-    // guarda segredos sobre o acidente de seus pais.");
 
-    // System.out.println("\nTaka entra na sala e pergunta:");
-    // System.out.println("\"Tudo bem com você, Nys?\"");
-    // System.out.println("1 - Responder amigavelmente");
-    // System.out.println("2 - Responder de forma suspeita");
-    // System.out.print("Escolha uma opção: ");
-
-    // this.obterRespostaDoJogador();
-
-    // if (resposta.equalsIgnoreCase("1")) {
-    // System.out.println("Você responde amigavelmente, mas, por dentro, continua
-    // desconfiando.");
-    // nivelDeConfianca += 2;
-    // } else if (resposta.equalsIgnoreCase("2")) {
-    // System.out.println("Você responde de forma suspeita. Taka observa, mas não
-    // reage.");
-    // nivelDeConfianca -= 2;
-    // }
-
-    // System.out.println(
-    // "\nMais tarde naquela noite, você encontra um arquivo no seu computador com o
-    // nome dos seus pais. O arquivo está encriptado.");
-    // System.out.println("1 - Tentar hackear o arquivo agora");
-    // System.out.println("2 - Perguntar a Taka sobre o arquivo");
-    // System.out.print("Escolha uma opção: ");
-    // this.obterRespostaDoJogador();
-
-    // if (resposta.equalsIgnoreCase("1")) {
-    // System.out.println(
-    // "Você tenta hackear o arquivo, mas percebe que o nível de encriptação é alto
-    // demais para suas habilidades atuais.");
-    // System.out.println("Você precisará estudar mais antes de tentar novamente.");
-    // } else if (resposta.equalsIgnoreCase("1")) {
-    // System.out.println("Você pergunta a Taka sobre o arquivo.");
-    // if (nivelDeConfianca > 5) {
-    // System.out.println("Taka hesita, mas responde: \"É apenas um arquivo antigo.
-    // Não se preocupe com isso.\"");
-    // } else {
-    // System.out.println(
-    // "Taka responde friamente: \"Não tenho informações sobre isso.\" Você sente
-    // que ele está escondendo algo.");
-    // nivelDeConfianca -= 1;
-    // }
-    // }
-
-    // System.out.println(
-    // "\nFrustrado, você decide começar a desenvolver um programa em Java para
-    // desativar temporariamente Taka e acessar os arquivos escondidos.");
-    // System.out.println("1 - Trabalhar no programa durante toda a noite");
-    // System.out.println("2 - Desistir e esperar outro dia");
-    // System.out.print("Escolha uma opção: ");
-    // this.obterRespostaDoJogador();
-
-    // if (resposta.equalsIgnoreCase("1")) {
-    // System.out.println(
-    // "Você passa a noite escrevendo linhas de código, focado em criar um programa
-    // que possa desativar Taka.");
-    // System.out.println("Sua habilidade de programação aumenta.");
-    // } else if (resposta.equalsIgnoreCase("2")) {
-    // System.out.println(
-    // "Você decide deixar para outro dia. Uma sensação de incerteza te atormenta
-    // enquanto você tenta dormir.");
-    // }
-
-    // System.out.println(
-    // "\nEssa é apenas a primeira noite de uma jornada sombria para descobrir a
-    // verdade sobre o acidente de seus pais.");
-    // System.out.println("Escolhas futuras moldarão o destino de Nys e sua relação
-    // com Taka...");
+    System.out.println(
+        "\nEssa é apenas a primeira noite de uma jornada sombria para descobrir a verdade sobre o acidente de seus pais.");
+    System.out.println("Escolhas futuras moldarao o destino de Nys e sua relacao com Taka...");
   }
 }
