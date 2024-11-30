93c10b761a3014463aab98d033029c09c0bbb70c
Ronaldo Junior <ronaldomjunior05@gmail.com>
2024-11-30 17:50:55 -0300

    chore: contributors names

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,11 @@
+// Grupo 5, Turma A
+//
+// Integrantes:
+//
+// Ronaldo Mota da Silva Junior
+// Samira Mayumi Yano
+// Vinicius Teixeira
+// Weliclene Inacio Pedroza
 
 public class Main {
   public static void main(String[] args){
